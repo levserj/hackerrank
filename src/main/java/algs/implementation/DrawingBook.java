3eac8718d8ac141c96ae3b7d624f4239751465e8
Serhii Levchynskyi <sl@norse.digital>
2018-04-27 17:58:46 +0300

    - next challenge

---
+++ src/main/java/algs/implementation/DrawingBook.java
@@ -0,0 +1,15 @@
+package algs.implementation;
+
+/**
+ * @author Sergiy Levchynskyi
+ */
+
+public class DrawingBook {
+    static int pageCount(int n, int p) {
+        int pages = 0;
+        /*
+         * Write your code here.
+         */
+        return pages;
+    }
+}
