d001a07db7d00e7dfa58e3a06b11e6270fda8b07
Serhii Levchynskyi <sl@norse.digital>
2018-04-30 11:50:20 +0300

    - next challenge

--- src/main/java/algs/implementation/DrawingBook.java
+++ src/main/java/algs/implementation/DrawingBook.java
@@ -7,9 +7,22 @@ package algs.implementation;
 public class DrawingBook {
     static int pageCount(int n, int p) {
         int pages = 0;
-        /*
-         * Write your code here.
-         */
+        if (p == 1 || p == n || (n % 2 == 1 && p > n - 2)) {
+            return pages;
+        }
+        if (p <= n / 2) {
+            pages = p / 2;
+        } else {
+            if (n % 2 == 0) {
+                pages = (n - p + 1) / 2;
+            } else {
+                pages = (n - p) / 2;
+            }
+        }
         return pages;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pageCount(8, 3));
     }
 }
