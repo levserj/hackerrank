540f19b7b7dfe26e23cdff1ae4f766ad89669c59
Serhii Levchynskyi <sl@norse.digital>
2019-12-13 15:49:10 +0200

    - next challenge

--- src/main/java/algs/arrays/NewYearChaos.java
+++ src/main/java/algs/arrays/NewYearChaos.java
@@ -1,26 +1,24 @@
 package algs.arrays;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class NewYearChaos {
 
     static void minimumBribes(int[] q) {
         int swaps = 0;
         String result = null;
-        Map<Integer, Integer> mapValueIndex = new HashMap<>();
-        for (int i = 0; i < q.length; i++) {
-            mapValueIndex.put(q[i], i + 1);
-        }
-        for (int i = 0; i < q.length; i++) {
-            if (q[i] != i + 1) {
-                int diffInPosition = mapValueIndex.get(q[i]) - i;
-                if (diffInPosition > 2) {
-                    result = "Too chaotic";
-                    break;
-                }
-                swaps += diffInPosition;
+
+        for (int i = q.length - 1; i >= 0; i--) {
+            int diffInPosition = q[i] - (i + 1);
+            if (diffInPosition > 2) {
+                result = "Too chaotic";
+                break;
             }
+
+            for (int j = Math.max(q[i] - 2, 0); j < i; j++) {
+                if (q[j] > q[i]) {
+                    swaps++;
+                }
+            }
+
         }
         if (null == result) {
             result = String.valueOf(swaps);
@@ -30,5 +28,8 @@ public class NewYearChaos {
 
     public static void main(String[] args) {
         minimumBribes(new int[]{2, 1, 5, 3, 4});
+        minimumBribes(new int[]{2, 5, 1, 3, 4});
+        minimumBribes(new int[]{1, 2, 5, 3, 7, 8, 6, 4});
+        minimumBribes(new int[]{5, 1, 2, 3, 7, 8, 6, 4});
     }
 }
